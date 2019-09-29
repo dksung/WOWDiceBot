54e737565def9c89c384835630be022de26d6afb
dksung <disung@acuodigital.com>
2019-09-29 22:13:27 +0900

    - 방 병로 주사위 순위를 분리하기 위한 List 추가.  - 로또 기능 추가.

--- WowDiceBot/WowDiceBot/Form1.cs
+++ WowDiceBot/WowDiceBot/Form1.cs
@@ -8,32 +8,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace WowDiceBot
 {
     public partial class Form1 : Form
     {
-        private bool startCheck = false;
         // int : ID
         // Tuple : 주사위값, UserName
-        //private Dictionary<int, string[]> keyValuePairs = new Dictionary<int, string[]>();
         private Dictionary<int, Tuple<int, string>> keyValuePairs = new Dictionary<int, Tuple<int, string>>();
+
+        private List<long> rankList = new List<long>();
+
+        private TelegramBotClient bot;
+        string token = string.Empty;
 
         public Form1()
         {
             InitializeComponent();
+            //@wow_dice_bot : 965378885:AAECg689Kuz-j819Yz84H4ZJu636ruXPu9s
+            //@clienDice_bot : 915917363:AAFNXIaWNGJE3nq2f0vUyeTk1vL1F516RUg;
+
+            token = @"965378885:AAECg689Kuz-j819Yz84H4ZJu636ruXPu9s";
+            //token = @"915917363:AAFNXIaWNGJE3nq2f0vUyeTk1vL1F516RUg";
+
+            bot = new TelegramBotClient(token);
         }
 
-        //@wow_dice_bot : 965378885:AAECg689Kuz-j819Yz84H4ZJu636ruXPu9s
-        private TelegramBotClient bot = new TelegramBotClient("965378885:AAECg689Kuz-j819Yz84H4ZJu636ruXPu9s");
-
-        //테스트용
-        //@clienDice_bot : 915917363:AAFNXIaWNGJE3nq2f0vUyeTk1vL1F516RUg;
-        //private TelegramBotClient bot = new TelegramBotClient("915917363:AAFNXIaWNGJE3nq2f0vUyeTk1vL1F516RUg");
-
-
         private void Form1_Load(object sender, EventArgs e)
-        {
+        { 
+            
             telegramAPIAsync();
             setEvent();
         }
@@ -64,6 +68,9 @@ namespace WowDiceBot
             if (inputText.StartsWith("/주사위"))
             {
                 bool rollDice = true;
+                bool startCheck = false;
+                if (rankList.Contains(message.Chat.Id) == true)
+                    startCheck = true;
 
                 if (startCheck == true)
                 {
@@ -80,7 +87,7 @@ namespace WowDiceBot
                     Random random = new Random();
                     int returnValue = 0;
                     int maxValue = 100;
-                    if (splitedText.Count() > 1)
+                    if (splitedText.Count() > 1 && startCheck == false)
                     {
                         if (int.TryParse(splitedText[1], out maxValue) == false)
                         {
@@ -89,62 +96,122 @@ namespace WowDiceBot
                     }
 
                     returnValue = random.Next(1, maxValue);
-                    var messageInfo = message.From;
-                    //string username = string.Format(@"@{0} ({1}{2})", messageInfo.Username, messageInfo.LastName, messageInfo.FirstName);
-                    string username = string.Format(@"@{0}", messageInfo.Username);
-
-                    if (string.IsNullOrEmpty(messageInfo.Username))
-                    {
-                        username = string.Format(@"{0}{1}", messageInfo.LastName, messageInfo.FirstName);
-                    }
-
-                    Console.WriteLine($"LastName : {messageInfo.LastName}");
-                    Console.WriteLine($"FirstName : {messageInfo.FirstName}");
+                    string username = GetDisplayname(message.From);
 
                     formatedText = string.Format(@"{0} 이(가) 주사위를 굴려 {1}이 나왔습니다.(1-{2})", username, returnValue, maxValue);
                     if (startCheck == true)
                     {
-                        keyValuePairs.Add(messageInfo.Id, new Tuple<int, string>(returnValue, username));
+                        keyValuePairs.Add(message.From.Id, new Tuple<int, string>(returnValue, username));
                     }
                     await bot.SendTextMessageAsync(message.Chat.Id, formatedText);
                 }
             }
             else if (inputText.StartsWith("/시작"))
             {
-                startCheck = true;
-                keyValuePairs.Clear();
-                await bot.SendTextMessageAsync(message.Chat.Id, "지금부터 굴리는 주사위 값에 순위를 매깁니다.\n주사위는 한 번 씩만 굴릴 수 있습니다.");
+                if(rankList.Contains(message.Chat.Id) == false)
+                {
+                    rankList.Add(message.Chat.Id);
+                    keyValuePairs.Clear();
+                    await bot.SendTextMessageAsync(message.Chat.Id, "지금부터 굴리는 주사위 값에 순위를 매깁니다.\n주사위는 한 번 씩만 굴릴 수 있습니다.\n주사위 값은 1과 100 사이의 값으로 강제합니다.");
+                }
+                else
+                {
+                    await bot.SendTextMessageAsync(message.Chat.Id, "결과 값을 먼저 확인 후 다시 시작해주세요.");
+                }
             }
             else if (inputText.StartsWith("/결과"))
             {
-                startCheck = false;
-                if (keyValuePairs.Count() == 0)
+                if (rankList.Contains(message.Chat.Id) == true)
                 {
-                    await bot.SendTextMessageAsync(message.Chat.Id, "아무도 주사위를 굴리지 않았습니다\n/시작을 한 뒤에 주사위를 굴려주세요");
-                    return;
+                    if (keyValuePairs.Count() == 0)
+                    {
+                        await bot.SendTextMessageAsync(message.Chat.Id, "아무도 주사위를 굴리지 않았습니다\n시작을 한 뒤에 주사위를 굴려주세요");
+                        return;
+                    }
+                    rankList.Remove(message.Chat.Id);
+
+                    
+                    await bot.SendTextMessageAsync(message.Chat.Id, GetResultValue());
                 }
-                var orderedItem = keyValuePairs.OrderByDescending(t => t.Value.Item1);
-
-                StringBuilder sb = new StringBuilder();
-                sb.AppendLine(@"******** 결과를 보여드립니다 ********");
-
-                int rank = 1;
-                foreach (var item in orderedItem)
+                else
                 {
-                    sb.AppendLine(string.Format($"{rank++}위 : \t {item.Value.Item2} \t {item.Value.Item1}"));
+                    await bot.SendTextMessageAsync(message.Chat.Id, "시작을 하지 않았습니다.");
                 }
-                keyValuePairs.Clear();
-                await bot.SendTextMessageAsync(message.Chat.Id, sb.ToString());
+            }
+            else if (inputText.StartsWith("/로또"))
+            {
+                int generateCount = 1;
+                if (splitedText.Count() > 1)
+                {
+                    if (int.TryParse(splitedText[1], out generateCount) == false)
+                    {
+                        generateCount = 1;
+                    }
+                }
+                
+                await bot.SendTextMessageAsync(message.Chat.Id, GetDisplayname(message.From) + " 님의 로또 예상 번호는\n" + GetLottoNumbers(generateCount) + " 입니다.");
             }
             else if (inputText.StartsWith("/도움말"))
             {
                 // 도움말 출력
-                await bot.SendTextMessageAsync(message.Chat.Id, "도움말 제작 예정");
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(@"현재 지원 기능은 아래와 같습니다.");
+                sb.AppendLine(@"/로또 : 랜덤한 로또 번호 6자리 생성");
+                sb.AppendLine(@"/주사위 : 1부터 100사이의 랜덤한 값 생성");
+                sb.AppendLine(@"/시작 : 주사위 순위 기능");
+                sb.AppendLine(@"/결과 : 주사위 순위 보기");
+
+                await bot.SendTextMessageAsync(message.Chat.Id, sb.ToString());
             }
-            else
+        }
+
+        private string GetLottoNumbers(int count)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
             {
-                await bot.SendTextMessageAsync(message.Chat.Id, inputText + " 는 미지원 명령어");
+                var set = new HashSet<int>();
+
+                while (set.Count() < 6)
+                {
+                    Random random = new Random();
+                    set.Add(random.Next(1, 46));
+                }
+                sb.AppendLine(string.Join(", ", set.OrderBy(t => t).Select(n => n.ToString())));
             }
+
+            return sb.ToString();
+        }
+
+        private string GetResultValue()
+        {
+            var orderedItem = keyValuePairs.OrderByDescending(t => t.Value.Item1);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(@"********** 순위 **********");
+            
+            int rank = 1;
+            foreach (var item in orderedItem)
+            {
+                sb.AppendLine(string.Format($"{rank++}위 : \t\t {item.Value.Item2} \t\t {item.Value.Item1}"));
+            }
+
+            sb.AppendLine(@"************************");
+            keyValuePairs.Clear();
+            return sb.ToString();
+        }
+
+        private string GetDisplayname(User user)
+        {
+            string username = string.Format(@"@{0}", user.Username);
+
+            if (string.IsNullOrEmpty(user.Username))
+            {
+                username = string.Format(@"{0}{1}", user.LastName, user.FirstName);
+            }
+
+            return username;
         }
     }
 }
